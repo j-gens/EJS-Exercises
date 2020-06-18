8299103e8b009b3ddddfb2e28d3bddcf4d9afadf
j-gens <jgens2@gmail.com>
2020-06-18 10:17:40 -0700

    Complete Ch 3 exercises

---
+++ CH_03_Functions.js
@@ -0,0 +1,30 @@
+// 3.1 Minimum
+const min = (a, b) => {
+  if (a < b) return a;
+  return b;
+}
+
+// 3.2 Recursion
+const isEven = (n) => {
+  if (n < 0) n = -n;
+  if (n === 0) return true;
+  if (n === 1) return false;
+  return isEven(n -2);
+}
+
+// 3.3 Bean Counting
+const countBs = (string) => {
+  let count = 0;
+  for (let char of string) {
+    if (char === 'B') count++;
+  }
+  return count;
+}
+
+const countChar = (string, target) => {
+  let count = 0;
+  for (let char of string) {
+    if (char === target) count++;
+  }
+  return count;
+}
