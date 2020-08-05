c351bd16b12f187f25e6e856586858e84a4f8702
j-gens <jgens2@gmail.com>
2020-08-05 15:01:11 -0700

    Start ch 9 regex exercises

---
+++ CH_09_Regex.js
@@ -0,0 +1,17 @@
+// 9.1 Regexp Golf
+//car and cat
+/ca(r|t)/
+// pop and prop
+/pr?op/
+// ferret, ferry, and ferrari
+/ferr(et|y|ari)/
+// Any word ending in ious
+/ious\b/
+// A whitespace character followed by a period, comma, colon, or semicolon
+/\s(.|,|;)/
+// A word longer than six letters
+/\w{6,}/
+// A word without the letter e (or E)
+/[^eE]/
+
+// 9.2
